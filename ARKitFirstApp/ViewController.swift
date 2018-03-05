3dac85e8a9d582156d3b1d1f2be021ec2c05ddc6
[gustavosmr96] <[gustavosmr96@icloud.com]>
2018-03-05 16:13:55 -0300

    Primeiro commit

--- ARKitFirstApp/ViewController.swift
+++ ARKitFirstApp/ViewController.swift
@@ -14,6 +14,19 @@ class ViewController: UIViewController, ARSCNViewDelegate {
 
     @IBOutlet var sceneView: ARSCNView!
     
+    var trackerNode: SCNNode!
+    var mainContainer: SCNNode!
+    var gameHasStarted = false
+    var foundSurface = false
+    var gamePos = SCNVector3Make(0.0, 0.0, 0.0)
+    var scoreLbl: UILabel!
+    
+    var score = 0 {
+        didSet {
+            scoreLbl.text = "\(score)"
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -24,7 +37,7 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         sceneView.showsStatistics = true
         
         // Create a new scene
-        let scene = SCNScene(named: "art.scnassets/ship.scn")!
+        let scene = SCNScene(named: "art.scnassets/Scene.scn")!
         
         // Set the scene to the view
         sceneView.scene = scene
@@ -47,34 +60,152 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         sceneView.session.pause()
     }
     
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Release any cached data, images, etc that aren't in use.
-    }
-
-    // MARK: - ARSCNViewDelegate
-    
-/*
-    // Override to create and configure nodes for anchors added to the view's session.
-    func renderer(_ renderer: SCNSceneRenderer, nodeFor anchor: ARAnchor) -> SCNNode? {
-        let node = SCNNode()
-     
-        return node
-    }
-*/
-    
-    func session(_ session: ARSession, didFailWithError error: Error) {
-        // Present an error message to the user
+    func randomPosition() -> SCNVector3 {
+        let randX = (Float(arc4random_uniform(200)) / 100.0) - 1.0
+        let randY = (Float(arc4random_uniform(200)) / 100.0) + 1.5
         
+        return SCNVector3Make(randX, randY, -3.0)
     }
     
-    func sessionWasInterrupted(_ session: ARSession) {
-        // Inform the user that the session has been interrupted, for example, by presenting an overlay
+    @objc func addPlane(){
+        let planeNode = sceneView.scene.rootNode.childNode(withName: "plane", recursively: false)?.copy() as! SCNNode
+        planeNode.isHidden = false
+        planeNode.position = randomPosition()
         
+        mainContainer.addChildNode(planeNode)
+        
+        let randSpeed = SCNVector3Make(0.0, 0.0, Float(arc4random_uniform(2) + 4))
+        planeNode.physicsBody = SCNPhysicsBody(type: .dynamic, shape: nil)
+        planeNode.physicsBody?.isAffectedByGravity = false
+        planeNode.physicsBody?.applyForce(randSpeed, asImpulse: true)
+        
+        let planeDissapearAction = SCNAction.sequence([SCNAction.wait(duration: 10.0), SCNAction.fadeOut(duration: 1.0), SCNAction.removeFromParentNode()])
+        planeNode.runAction(planeDissapearAction)
+        
+        Timer.scheduledTimer(timeInterval: 2.0, target: self, selector: #selector(addPlane), userInfo: nil, repeats: false)
+    }
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        if gameHasStarted {
+            //guard let touch = touches.first else { return }
+            let touchLocation = CGPoint(x:view.frame.width/2, y: view.frame.height/2) //touch.location(in: view)
+            print(touchLocation)
+            guard let nodeHitTest = sceneView.hitTest(touchLocation, options: nil).first else { return }
+            let hitNode = nodeHitTest.node
+            
+            guard hitNode.name == "plane" else { return }
+            score += 1
+            
+            let explosion = SCNParticleSystem(named: "Explosion.scnp", inDirectory: nil)!
+            hitNode.addParticleSystem(explosion)
+            
+            let planeSpinForce = SCNVector4Make(0.5, 0.0, 1.0, 1.0)
+            hitNode.physicsBody?.isAffectedByGravity = true
+            hitNode.physicsBody?.applyTorque(planeSpinForce, asImpulse: true)
+            
+        }else{
+            guard foundSurface else { return }
+            trackerNode.removeFromParentNode()
+            gameHasStarted = true
+            
+            scoreLbl = UILabel(frame: CGRect(x: 0.0, y: view.frame.height * 0.05, width: view.frame.width, height: view.frame.height * 0.1))
+            scoreLbl.textAlignment = .center
+            scoreLbl.font = UIFont(name: "Arial", size: view.frame.width * 0.1)
+            scoreLbl.textColor = .yellow
+            scoreLbl.text = "\(score)"
+            
+            view.addSubview(scoreLbl)
+            
+            mainContainer = sceneView.scene.rootNode.childNode(withName: "mainContainer", recursively: false)!
+            mainContainer.isHidden = false
+            mainContainer.position = gamePos
+         
+            addPlane()
+            
+            let ambientLight = SCNLight()
+            ambientLight.type = .ambient
+            ambientLight.color = UIColor.white
+            ambientLight.intensity = 500
+            
+            let ambientLightNode = SCNNode()
+            ambientLightNode.light = ambientLight
+            ambientLightNode.position.y = 3.0
+            
+            mainContainer.addChildNode(ambientLightNode)
+            
+            let omniLight = SCNLight()
+            omniLight.type = .omni
+            omniLight.color = UIColor.white
+            omniLight.intensity = 1000
+            
+            let omniLightNode = SCNNode()
+            omniLightNode.light = omniLight
+            omniLightNode.position.y = 3.0
+            
+            mainContainer.addChildNode( omniLightNode)
+            
+        }
     }
     
-    func sessionInterruptionEnded(_ session: ARSession) {
-        // Reset tracking and/or remove existing anchors if consistent tracking is required
+    func renderer(_ renderer: SCNSceneRenderer, updateAtTime time: TimeInterval) {
+        guard !gameHasStarted else { return }
+        guard let hitTest = sceneView.hitTest(CGPoint(x: view.frame.midX, y: view.frame.midY), types: [.existingPlane, .featurePoint]).last else { return }
+        let trans = SCNMatrix4(hitTest.worldTransform)
+        gamePos = SCNVector3Make(trans.m41, trans.m42, trans.m43)
         
+        if !foundSurface {
+            let trackerPlane = SCNPlane(width: 0.3, height: 0.3)
+            trackerPlane.firstMaterial?.diffuse.contents = #imageLiteral(resourceName: "tracker")
+            trackerNode = SCNNode(geometry: trackerPlane)
+            print(trackerNode.position)
+            trackerNode.eulerAngles.x = .pi * -0.5
+            sceneView.scene.rootNode.addChildNode(trackerNode)
+        }
+        trackerNode.position = gamePos
+        foundSurface = true
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
